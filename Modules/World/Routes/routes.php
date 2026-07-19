f61956d4e80ead7cd660be1ae75ecff7fa150679
YasinMosaviAval <yasin.mosaviaval@gmail.com>
2026-07-19 11:35:31 +0330

    refactor(routes): remove framework test routes and organize module routes

--- Modules/World/Routes/routes.php
+++ Modules/World/Routes/routes.php
@@ -3,6 +3,13 @@
 use Core\Router\Router;
 use Modules\World\Controllers\Api\WorldController;
 
+
+Router::get(
+    '/api/world/provinces/{provinceId}/counties',
+    [WorldController::class,'counties']
+);
+
+
 Router::post(
     '/api/world/google-address',
     [WorldController::class,'googleAddress']
