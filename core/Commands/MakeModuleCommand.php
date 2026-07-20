4417697614a08b259a571b38420312df3c938c28
YasinMosaviAval <yasin.mosaviaval@gmail.com>
2026-07-20 18:15:29 +0330

    feat(module-generator): generate default module views and assets

--- core/Commands/MakeModuleCommand.php
+++ core/Commands/MakeModuleCommand.php
@@ -120,11 +120,24 @@ class MakeModuleCommand extends Command {
             'layout.stub' => 'Resources/Views/layouts/main.php',
             /*
             |--------------------------------------------------------------------------
-            | Assets
+            | CSS
             |--------------------------------------------------------------------------
             */
-            'style.stub' => 'Resources/Assets/css/style.css',
-            'script.stub' => 'Resources/Assets/js/script.js',
+            'style.stub'       => 'Resources/Assets/css/style.css',
+            'index.css.stub'   => 'Resources/Assets/css/index.css',
+            'create.css.stub'  => 'Resources/Assets/css/create.css',
+            'edit.css.stub'    => 'Resources/Assets/css/edit.css',
+            'show.css.stub'    => 'Resources/Assets/css/show.css',
+            /*
+            |--------------------------------------------------------------------------
+            | Javascript
+            |--------------------------------------------------------------------------
+            */
+            'script.stub'     => 'Resources/Assets/js/script.js',
+            'index.js.stub'   => 'Resources/Assets/js/index.js',
+            'create.js.stub'  => 'Resources/Assets/js/create.js',
+            'edit.js.stub'    => 'Resources/Assets/js/edit.js',
+            'show.js.stub'    => 'Resources/Assets/js/show.js',
             /*
             |--------------------------------------------------------------------------
             | Module Files
